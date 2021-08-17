29e488f94af92b09993bf31574245af52985d523
thkummi <36160753+thkummi@users.noreply.github.com>
2021-08-17 02:44:03 +0200

    Update j-pole-calc.c

--- j-pole-calc.c
+++ j-pole-calc.c
@@ -1,33 +1,16 @@
 /*
-BSD 3-Clause License
-
-Copyright (c) 2021, Thomas Kummer
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of the copyright holder nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+J-Pole Antanna Calculator
+Copyright (C) 2021  Thomas Kummer
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
@@ -36,12 +19,12 @@ int main() {
 	
 	float f, lambda, l1, l2, l3, l4, raddiam;
 
-	printf("Please input an f in MHz value: E.g. 432.2\n");
+	printf("Please input an f in MHz value: E.g. 432.2 ");
     	scanf("%f", &f);
     
     	printf("Calculating for %f MHz\n", f);
 	
-	lambda = ( 29971000/f)/100;	
+	lambda = ( 29971000/f)/100;
 	l1 = 0.96*lambda*0.75;
 	l2 = 0.96*lambda*0.25;
 	l3 = 0.96*lambda*0.025;
@@ -66,9 +49,9 @@ int main() {
 	printf("              [   ||                       ||       ]\n");
 	printf("              [   ||                       ||       ]\n");
 	printf("              [   ||                       ||     L2]%f mm\n", l2);
-	printf("              [   ||             ______    ||       ]\n");
-	printf("              [   ||         ___|___   \\   ||       ]\n");
-	printf("              [   ||________/'  |  '\\   \\__||       ]\n");
+	printf("              [   ||      ______           ||       ]\n");
+	printf("              [   ||     /   ___|___       ||       ]\n");
+	printf("              [   ||____/   /'  |  '\\______||       ]\n");
 	printf("              [   ||        \\.__|__./      ||    ]  ]\n");
 	printf("              [   ||        |  50   |      ||  L4]  ]%f mm\n", l4);
 	printf("              [   ||        |  Ohm  |      ||    ]  ]\n");
@@ -80,6 +63,6 @@ int main() {
 	printf("\n");
 	printf("\n");
 	printf("                             73! \n");
-	printf("\n");	
+	printf("\n");
 	return 0;
 }
